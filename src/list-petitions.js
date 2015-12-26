960a6b7a014971a556c719784801e479ee0038e0
Matt Champion <mattunderscorechampion@gmail.com>
2015-12-26 21:18:46 +0000

    Simplified list petitions code.

--- src/list-petitions.js
+++ src/list-petitions.js
@@ -1,11 +1,10 @@
 
 'use strict';
 
-var https = require("https"),
-    PetitionPager = require('./petition-pager'),
+var PetitionPager = require('./petition-pager'),
     output = require('./simple-output');
 
-var p = new PetitionPager();
-p.on('error', output.error)
+new PetitionPager()
+    .on('error', output.error)
     .on('petition', output.withSignatureCountDiff)
     .populateRecent();
