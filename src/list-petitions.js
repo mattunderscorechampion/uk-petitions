c0e74cc96473cfc1a9f2e94df81393da09d560e9
Matt Champion <mattunderscorechampion@gmail.com>
2015-12-26 21:15:00 +0000

    Added npm script to list recent petitions.

--- src/list-petitions.js
+++ src/list-petitions.js
@@ -2,15 +2,10 @@
 'use strict';
 
 var https = require("https"),
-    EventEmitter = require('events'),
     PetitionPager = require('./petition-pager'),
     output = require('./simple-output');
 
 var p = new PetitionPager();
 p.on('error', output.error)
     .on('petition', output.withSignatureCountDiff)
-    .on('recent-loaded', function () {
-        // Check the first page for any changes
-        p.populateRecent();
-    })
     .populateRecent();
