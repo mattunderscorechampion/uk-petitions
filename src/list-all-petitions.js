85312ab8b90514739e456738f75752988c4492a6
Matt Champion <mattunderscorechampion@gmail.com>
2015-12-26 22:09:14 +0000

    Added npm script to list all petitions.

---
+++ src/list-all-petitions.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+var PetitionPager = require('./petition-pager'),
+    output = require('./simple-output');
+
+new PetitionPager()
+    .on('error', output.error)
+    .on('petition', output.withSignatureCountDiff)
+    .populateAll();
