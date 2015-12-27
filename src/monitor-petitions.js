c071be386ed8ea9d1a0ae649d678d39bff8878bb
Matt Champion <mattunderscorechampion@gmail.com>
2015-12-27 14:20:42 +0000

    Simplified adding signature notifications.

--- src/monitor-petitions.js
+++ src/monitor-petitions.js
@@ -2,50 +2,37 @@
 'use strict';
 
 var Monitor = require('./petitions-monitor'),
-    output = require('./simple-output');
+    output = require('./simple-output'),
+    util = require('util');
 
-new Monitor()
-    .setMaxListeners(20)
+var m = new Monitor();
+m.addSignatureNotification = function(signatures) {
+    return this.on(util.format('reached-%d-signatures', signatures), function(data) {
+        console.log('Petition \'%s\' has reached %d signatures', data.attributes.action, signatures);
+    });
+};
+
+m.setMaxListeners(20)
     .on('error', output.error)
     .on('new-petition', function(data) {
         console.log('New petition \'%s\'', data.attributes.action);
     })
-    .on('reached-10-signatures', function(data) {
-        console.log('Petition \'%s\' has reached 10 signatures', data.attributes.action);
-    })
-    .on('reached-20-signatures', function(data) {
-        console.log('Petition \'%s\' has reached 20 signatures', data.attributes.action);
-    })
-    .on('reached-50-signatures', function(data) {
-        console.log('Petition \'%s\' has reached 50 signatures', data.attributes.action);
-    })
-    .on('reached-100-signatures', function(data) {
-        console.log('Petition \'%s\' has reached 100 signatures', data.attributes.action);
-    })
-    .on('reached-250-signatures', function(data) {
-        console.log('Petition \'%s\' has reached 250 signatures', data.attributes.action);
-    })
-    .on('reached-500-signatures', function(data) {
-        console.log('Petition \'%s\' has reached 500 signatures', data.attributes.action);
-    })
-    .on('reached-1000-signatures', function(data) {
-        console.log('Petition \'%s\' has reached 1000 signatures', data.attributes.action);
-    })
-    .on('reached-5000-signatures', function(data) {
-        console.log('Petition \'%s\' has reached 5000 signatures', data.attributes.action);
-    })
+    .addSignatureNotification(10)
+    .addSignatureNotification(20)
+    .addSignatureNotification(50)
+    .addSignatureNotification(100)
+    .addSignatureNotification(250)
+    .addSignatureNotification(500)
+    .addSignatureNotification(1000)
+    .addSignatureNotification(5000)
     .on('reached-response-threshold', function(data) {
-        console.log('Petition \'%s\' has reached 10000 signatures', data.attributes.action);
+        console.log('Petition \'%s\' has reached the threshold for a response', data.attributes.action);
     })
-    .on('reached-50000-signatures', function(data) {
-        console.log('Petition \'%s\' has reached 50000 signatures', data.attributes.action);
-    })
+    .addSignatureNotification(50000)
     .on('reached-debate-threshold', function(data) {
-        console.log('Petition \'%s\' has reached 100000 signatures', data.attributes.action);
+        console.log('Petition \'%s\' has reached the threshold for a debate', data.attributes.action);
     })
-    .on('reached-500000-signatures', function(data) {
-        console.log('Petition \'%s\' has reached 500000 signatures', data.attributes.action);
-    })
+    .addSignatureNotification(500000)
     .on('government-responce', function(data) {
         console.log('The government has responded to the petition \'%s\'', data.attributes.action);
     })
