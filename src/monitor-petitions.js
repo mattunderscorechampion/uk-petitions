3a07da1ade2ee54d246e3f75e76b142e6bf29e78
Matt Champion <mattunderscorechampion@gmail.com>
2016-01-08 23:14:20 +0000

    Use the enriched petitions in monitor example.

--- src/monitor-petitions.js
+++ src/monitor-petitions.js
@@ -8,12 +8,12 @@ var Monitor = require('./petitions-monitor'),
 
 module.exports = {
     start : function() {
-        var m = new Monitor({debug: false, loadDetail: false});
+        var m = new Monitor({debug: false, loadDetail: false, enrich : true});
         m.addSignatureNotification = function(signatures) {
             var event = util.format('reached-%d-signatures', signatures);
             this.addMonitorDeltaEvent(event, queries.checks.delta.reachedSignatureDeltaCountProvider(signatures));
             return this.on(event, function(data) {
-                console.log('Petition \'%s\' has reached %d signatures', data.attributes.action, signatures);
+                console.log('Petition \'%s\' has reached %d signatures', data.action, signatures);
             });
         };
 
@@ -28,10 +28,10 @@ module.exports = {
             m
             .removeAllListeners('initial-load')
             .on('new-petition', function(data) {
-                console.log('New petition \'%s\'', data.attributes.action);
+                console.log('New petition \'%s\'', data.action);
             })
             .on('updated-petition', function(data) {
-                console.log('Updated petition \'%s\' has %d signatures', data.attributes.action, data.attributes.signature_count);
+                console.log('Updated petition \'%s\' has %d signatures', data.action, data.signature_count);
             })
             .addSignatureNotification(10)
             .addSignatureNotification(20)
@@ -42,18 +42,18 @@ module.exports = {
             .addSignatureNotification(1000)
             .addSignatureNotification(5000)
             .on('reached-response-threshold', function(data) {
-                console.log('Petition \'%s\' has reached the threshold for a response', data.attributes.action);
+                console.log('Petition \'%s\' has reached the threshold for a response', data.action);
             })
             .addSignatureNotification(50000)
             .on('reached-debate-threshold', function(data) {
-                console.log('Petition \'%s\' has reached the threshold for a debate', data.attributes.action);
+                console.log('Petition \'%s\' has reached the threshold for a debate', data.action);
             })
             .addSignatureNotification(500000)
             .on('government-response', function(data) {
-                console.log('Response to \'%s\' https://petition.parliament.uk/petitions/%d', data.attributes.action, data.id);
+                console.log('Response to \'%s\' https://petition.parliament.uk/petitions/%d', data.action, data.id);
             })
             .on('debate-transcript', function(data) {
-                console.log('Debate of \'%s\' %s', data.attributes.action, data.attributes.debate.transcript_url);
+                console.log('Debate of \'%s\' %s', data.action, data.debate.transcript_url);
             });
         })
         .start();
