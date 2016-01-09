32b4df2ae7f2f61b904d6c530343f09e13365402
Matt Champion <mattunderscorechampion@gmail.com>
2016-01-09 12:18:15 +0000

    Added additional properties derived from others.

--- src/enriched-petition.js
+++ src/enriched-petition.js
@@ -34,6 +34,11 @@ var keysToTransform = {
  * @property {Date} response_threshold_reached_at - The timestamp 10000 signatures was reached.
  * @property {Date} debate_threshold_reached_at - The timestamp 100000 signatures was reached.
  * @property {Date} scheduled_debate_date - The date the debate has been scheduled for.
+ * @property {string} html_url - URL for HTML version of the petition.
+ * @property {string} json_url - URL for JSON version of the petition.
+ * @property {string} html_detail_url - URL for HTML version of the petition showing the additional_details.
+ * @property {string} html_response_url - URL for HTML version of the petition showing the response.
+ * @property {string} html_debate_url - URL for HTML version of the petition showing the debate.
  */
 function EnrichedPetition(rawPetition) {
     this.id = rawPetition.id;
@@ -48,6 +53,11 @@ function EnrichedPetition(rawPetition) {
             this[key] = rawPetition.attributes[key];
         }
     }
+    this.html_url = util.format('https://petition.parliament.uk/petitions/%d', this.id);
+    this.json_url = util.format('https://petition.parliament.uk/petitions/%d.json', this.id);
+    this.html_detail_url = util.format('https://petition.parliament.uk/petitions/%d#details-content-0', this.id);
+    this.html_response_url = util.format('https://petition.parliament.uk/petitions/%d?reveal_response=yes#response-threshold', this.id);
+    this.html_debate_url = util.format('https://petition.parliament.uk/petitions/%d?#debate-threshold', this.id);
 }
 
 module.exports = EnrichedPetition;
