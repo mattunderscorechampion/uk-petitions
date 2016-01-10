3d8bb0b318f442562d04807f4650df552b07b72a
Matt Champion <mattunderscorechampion@gmail.com>
2016-01-10 23:42:29 +0000

    Added tests for Latch.

---
+++ test/latch.js
@@ -0,0 +1,33 @@
+
+'use strict';
+
+describe("Latch", function() {
+    var Latch = require('../src/latch');
+
+    it('released if no count down', function(done) {
+        var latch = new Latch(0);
+        latch.onRelease(done);
+    });
+    it('released after count down', function(done) {
+        var latch = new Latch(1);
+        latch.onRelease(done);
+        latch.release();
+    });
+    it('stays released after count down', function(done) {
+        var latch = new Latch(1);
+        latch.release();
+        latch.onRelease(done);
+    });
+    it('released after long count down', function(done) {
+        var latch = new Latch(2);
+        latch.onRelease(done);
+        latch.release();
+        latch.release();
+    });
+    it('stays released after long count down', function(done) {
+        var latch = new Latch(2);
+        latch.release();
+        latch.release();
+        latch.onRelease(done);
+    });
+});
