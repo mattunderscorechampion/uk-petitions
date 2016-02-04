d5a5fd746e8fa461b74298c63b22a5585df7f767
Matt Champion <mattunderscorechampion@gmail.com>
2016-02-04 20:47:08 +0000

    Improved build by increasing parallelism.

--- gulpfile.js
+++ gulpfile.js
@@ -23,20 +23,22 @@ gulp.task('generate', function() {
         .pipe(gulp.dest('target/js'));
 });
 
-gulp.task('checks', ['generate'], function() {
-    var checkJs = gulp.src(['./src/*.js', './src/examples/*.js'])
+gulp.task('js-checks', function() {
+    return gulp.src(['./src/*.js', './src/examples/*.js'])
         .pipe(jshint({node: true}))
         .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(jshint.reporter('fail'))
-
-    var checkTs = gulp.src('./src/*.ts')
-        .pipe(tslint())
-        .pipe(tslint.report('verbose'));
-
-    return merge(checkJs, checkTs);
+        .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('test', ['checks'], function(done) {
+gulp.task('ts-checks', ['generate'], function() {
+    return gulp.src('./src/*.ts')
+        .pipe(tslint())
+        .pipe(tslint.report('verbose'));
+});
+
+gulp.task('checks', ['js-checks', 'ts-checks']);
+
+gulp.task('test', ['generate'], function(done) {
     gulp.src(["./src/*.js"])
         .pipe(istanbul({
             includeUntested : true
@@ -55,10 +57,13 @@ gulp.task('test', ['checks'], function(done) {
         });
 });
 
-gulp.task('doc', ['test'], function() {
-    var docJs = gulp.src(['./src/*.js'])
+gulp.task('js-doc', function() {
+    return gulp.src(['./src/*.js'])
         .pipe(jsdoc('./target/doc'));
-    var docTs = gulp.src(['./src/ts/public/*.ts'])
+});
+
+gulp.task('ts-doc', ['generate'], function() {
+    return gulp.src(['./src/ts/public/*.ts'])
         .pipe(tsdoc({
             target : 'ES5',
             module : 'commonjs',
@@ -66,8 +71,8 @@ gulp.task('doc', ['test'], function() {
             excludeNotExported: true,
             out: './target/tsdoc'
         }));
-
-    return merge(docJs, docTs);
 });
+
+gulp.task('doc', ['js-doc', 'ts-doc']);
 
 gulp.task('default', ['generate', 'checks', 'test', 'doc']);
