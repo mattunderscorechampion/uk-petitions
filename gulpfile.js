1718c5aee8405a39c680331ce9c0d97078a380c7
Matt Champion <mattunderscorechampion@gmail.com>
2016-03-03 22:39:19 +0000

    Clean up the gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -23,34 +23,20 @@ gulp.task('generate', function() {
         .pipe(gulp.dest('target/js'));
 });
 
-gulp.task('ts-checks', ['generate'], function() {
+gulp.task('checks', ['generate'], function() {
     return gulp.src('./src/*.ts')
         .pipe(tslint())
         .pipe(tslint.report('verbose'));
 });
 
-gulp.task('checks', ['ts-checks']);
-
-gulp.task('test', ['generate'], function(done) {
-    gulp.src(["./src/*.js"])
-        .pipe(istanbul({
-            includeUntested : true
-        }))
-        .pipe(istanbul.hookRequire())
-        .on('finish', function() {
-            gulp.src(['./runner.js', 'test/*.js', 'test/**/*.js'])
-                .pipe(jasmine({
-                    includeStackTrace : true
-                 }))
-                .pipe(istanbul.writeReports({
-                    dir : "./target/coverage",
-                    reporters : ['cobertura', 'html']
-                }))
-                .on('end', done);
-        });
+gulp.task('test', ['generate'], function() {
+    return gulp.src('test/*.js')
+        .pipe(jasmine({
+            includeStackTrace : true
+        }));
 });
 
-gulp.task('ts-doc', function() {
+gulp.task('doc', function() {
     return gulp.src(['./src/ts/public/*.ts'])
         .pipe(tsdoc({
             target : 'ES5',
@@ -59,10 +45,8 @@ gulp.task('ts-doc', function() {
             mode : 'file',
             excludeNotExported: true,
             excludeExternals: true,
-            out: './target/tsdoc'
+            out: './target/doc'
         }));
 });
-
-gulp.task('doc', ['ts-doc']);
 
 gulp.task('default', ['generate', 'checks', 'test', 'doc']);
