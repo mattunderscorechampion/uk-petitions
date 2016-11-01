f7e81c93e4e16ce082c31a8ee5e62f9f1a041719
Matt Champion <mattunderscorechampion@gmail.com>
2016-11-01 21:15:11 +0000

    Create empty branch for gh-pages.

--- examples/list-hot-petitions.js
+++
@@ -1,9 +0,0 @@
-
-'use strict';
-
-var ukPetitions = require('../');
-
-new ukPetitions.PetitionPager()
-    .on('error', ukPetitions.output.error)
-    .on('petition', ukPetitions.output.withSignatureCountDiff)
-    .populateHot();
